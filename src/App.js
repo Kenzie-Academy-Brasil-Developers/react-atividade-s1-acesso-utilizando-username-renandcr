cc408c52249a0d5532810555f5d450a85839d61d
renandcr <renandamagazine@gmail.com>
2022-02-06 11:22:59 -0300

    Atividade finalizada

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import WelcomePage from "./components/WelcomePage";
-// import GetUserComponent from "./components/GetUserComponent";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,7 +20,6 @@ function App() {
         <WelcomePage
           login={HandleLogin}
           logout={HandleLogout}
-          // user={user}
           isLoggedIn={isLoggedIn}
         />
       </header>
